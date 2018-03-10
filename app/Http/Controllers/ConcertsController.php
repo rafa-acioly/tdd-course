a60c141865f8cffaf21b38c868abdd98e6ace6ea
Rafeal <aciolyr@gmail.com>
2018-03-10 18:49:47 -0300

    Primeiro teste de feature

---
+++ app/Http/Controllers/ConcertsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Concert;
+
+class ConcertsController extends Controller
+{
+    public function show($id)
+    {
+    	$concert = Concert::find($id);
+    	return view('concerts.show', ['concert' => $concert]);
+    }
+}
